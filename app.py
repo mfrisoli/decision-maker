245ea8ed9add29ad44167e75023c36bbf6e0b2d1
Marco Frisoli <marcof787@gmail.com>
2020-07-30 15:46:22 +0100

    modularize app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from cs50 import SQL
+from models import  app, db
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
@@ -8,12 +9,7 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, create_room
-
-# Configure application
-app = Flask(__name__)
-
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+#Users, Rooms, Options, Votes, Roomjoins,
 
 # Ensure responses aren't cached
 @app.after_request
@@ -22,15 +18,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///database.db")
 
 
 # Search for rooms
@@ -273,6 +260,7 @@ def register():
         hash_pass = generate_password_hash(password)
         
         # Check if user already exists
+        
         user_registered = db.execute("SELECT * FROM users WHERE users.username=:username", username=username)
 
         if len(user_registered) != 0:
--- models.py
+++ models.py
@@ -1,13 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
-from app import app
+from flask import Flask
+from tempfile import mkdtemp
+from flask_session import Session
+from cs50 import SQL
+
+# Configure application
+app = Flask(__name__)
 
 # Configuration Values
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///datab_2.db' # Tells SQLalchemy how to connect to the database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///datab_2.db' # Tells SQLalchemy how to connect to the database
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Institiate the Database Object
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
+
+"""
 class Users(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -51,7 +70,7 @@ class Roomjoins(db.Model):
     room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-
+"""
 
 
 
