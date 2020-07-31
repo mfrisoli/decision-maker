a27959b2841444b00399dc170a7a1a907eebc730
Marco Frisoli <marcof787@gmail.com>
2020-07-31 20:59:12 +0100

    bug fix

--- models.py
+++ models.py
@@ -24,9 +24,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Institiate the Database Object
