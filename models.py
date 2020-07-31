e973c5d6fd0e1d85daae8d0a63da81b3a70302dd
Marco Frisoli <marcof787@gmail.com>
2020-07-31 22:03:16 +0100

    test-1

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ from cs50 import SQL
 
 # Configure application
 app = Flask(__name__)
+app.secret_key = 'sdfs5df5sdfsgsdkgnsdlkfsdf5fdsd5fs5dfsdf4s5df5sd'
 
 ENVI = 'prod'
 
@@ -24,9 +25,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
-#app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "filesystem"
+
 Session(app)
 
 # Institiate the Database Object
